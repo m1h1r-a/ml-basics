6fbce557e4ec39ec2d57f11ab3e96afba65a0114
m1h1r-a <mihir.annambhotla@gmail.com>
2025-01-25 16:21:05 +0530

    basic linear regression model done

--- lr.py
+++ lr.py
@@ -10,4 +10,26 @@ data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 for i in range(2):
     print()
 
-print(data.head())
+# print(data.head())
+# print()
+
+predict = "G3"
+
+X = np.array(data.drop([predict], axis=1))
+Y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+    X, Y, test_size=0.1
+)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+
+acc = linear.score(x_test, y_test)
+print(f"Accuracy: {acc}")
+
+predictions = linear.predict(x_test)
+print()
+for x in range(len(predictions)):
+    print(f"{predictions[x]}  {y_test[x]} {x_test[x]}")
